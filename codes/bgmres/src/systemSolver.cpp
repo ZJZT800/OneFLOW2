ac685a6ea20123ebbae93187dc9e58d108f788d2
ZJZT800 <57002932+ZJZT800@users.noreply.github.com>
2020-10-12 11:16:34 +0800

    10.12(1e-12)

--- codes/bgmres/src/systemSolver.cpp
+++ codes/bgmres/src/systemSolver.cpp
@@ -51,7 +51,7 @@ void SolveMRhs::BGMRES()
 	Preconditioner* pre =
 		new Preconditioner(Rank.RANKNUMBER);      // The preconditioner for the system.
 	int restart = 0;                    // Number of restarts to allow
-	int maxIt = 2000;                      // Dimension of the Krylov subspace
+	int maxIt = 5000;                      // Dimension of the Krylov subspace
 	double tol = 1.0E-12;                 // How close to make the approximation.
 
 	/**
